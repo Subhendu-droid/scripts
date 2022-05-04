e4305ab8e44d2155ffd747b2a4620aac0c9d61a0
Subhendu-droid <55965922+Subhendu-droid@users.noreply.github.com>
2022-05-04 08:57:37 +0530

    Create build-aosp-clang.sh

---
+++ build-aosp-clang.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2020 Saalim Quadri (danascape)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+echo "Cloning dependencies"
+git clone --depth=1 https://github.com/Shubhendu-XD/android_kernel_asus_X00P -b lineage-18.x kernel
+cd kernel
+git clone --depth=1 https://github.com/stormbreaker-project/stormbreaker-clang clang
+git clone --depth=1 https://github.com/stormbreaker-project/aarch64-linux-android-4.9 gcc
+git clone --depth=1 https://github.com/stormbreaker-project/arm-linux-androideabi-4.9 gcc32
+git clone --depth=1 https://github.com/stormbreaker-project/AnyKernel3 -b X00P
+echo "Done"
+GCC="$(pwd)/aarch64-linux-android-"
+TANGGAL=$(date +"%F-%S")
+TOKEN="5096348570:AAG7Q303c2uzWyi4I1oGND6uA0EuS8gO2-A"
+chat_id=-1001596395081
+START=$(date +"%s")
+export CONFIG_PATH=$PWD/arch/arm64/configs/X00P_defconfig
+PATH="${PWD}/clang/bin:${PWD}/gcc/bin:${PWD}/gcc32/bin:${PATH}"
+export ARCH=arm64
+export KBUILD_BUILD_USER="danascape"
+export KBUILD_BUILD_HOST="StormCI"
+
+# Send info to channel
+function sendinfo() {
+	curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
+		-d chat_id="$chat_id" \
+		-d "disable_web_page_preview=true" \
+		-d "parse_mode=html" \
+		-d text="<b>• StormbreakerKernel •</b>%0ABuild started on <code>StormCI</code>%0AFor device <b>Zenfone Max M1</b> (X00P)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #Test"
+}
+
+# Push kernel to channel
+function push() {
+	cd AnyKernel3
+	ZIP=$(echo *.zip)
+	curl -F document=@$ZIP "https://api.telegram.org/bot$TOKEN/sendDocument" \
+		-F chat_id="$chat_id" \
+		-F "disable_web_page_preview=true" \
+		-F "parse_mode=html" \
+		-F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Zenfone Max M1 (X00P)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+}
+
+# spam Error
+function finerr() {
+	curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
+		-d chat_id="$chat_id" \
+		-d "disable_web_page_preview=true" \
+		-d "parse_mode=markdown" \
+		-d text="Build throw an error(s)"
+	exit 1
+}
+
+# Compile
+function compile() {
+	make O=out ARCH=arm64 X00P_defconfig
+	make -j$(nproc --all) O=out \
+		ARCH=arm64 \
+		CROSS_COMPILE=aarch64-linux-android- \
+		CROSS_COMPILE_ARM32=arm-linux-androideabi-
+	cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+}
+
+# Zipping
+function zip() {
+	cd AnyKernel3 || exit 1
+	zip -r9 Stormbreaker-X00P-${TANGGAL}.zip *
+	cd ..
+}
+sendinfo
+compile
+zip
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
